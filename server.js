ed3cb0eb7a15fa08a7ea9ec04ba1dcc07526222e
Rob Hess <robwhess@gmail.com>
2017-11-29 14:34:18 -0800

    Add code to take mongo db info from env.

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ var peopleData = require('./peopleData');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = process.env.MONGO_DB;
+var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword +
+  '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
