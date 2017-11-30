78a0427a9042226dfca3d6ea4e358eac47f22480
Rob Hess <robwhess@gmail.com>
2017-11-29 16:38:51 -0800

    Add code from 11/29 lecture.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 
 var peopleData = require('./peopleData');
 var app = express();
@@ -12,8 +13,13 @@ var mongoPort = process.env.MONGO_PORT || 27017;
 var mongoUser = process.env.MONGO_USER;
 var mongoPassword = process.env.MONGO_PASSWORD;
 var mongoDBName = process.env.MONGO_DB;
+
 var mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword +
   '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
+var mongoConnection = null;
+
+// console.log("== Mongo URL:", mongoURL);
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -68,6 +74,12 @@ app.post('*', function (req, res) {
   res.status(404).send("POST to unknown path");
 });
 
-app.listen(port, function () {
-  console.log("== Server listening on port:", port);
+MongoClient.connect(mongoURL, function (err, connection) {
+  if (err) {
+    throw err;
+  }
+  mongoConnection = connection;
+  app.listen(port, function () {
+    console.log("== Server listening on port:", port);
+  });
 });
